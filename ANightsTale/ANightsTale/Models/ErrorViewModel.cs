d1bc4dce37e3eb179d171493511dc676b8391982
stormrubio3000 <storm-3000@hotmail.com>
2019-03-14 18:03:27 -0500

    Framework push

---
+++ ANightsTale/ANightsTale/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ANightsTale.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
