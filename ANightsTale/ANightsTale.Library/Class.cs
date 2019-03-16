f7a27ef6bd652e68b1650e02da557750207c7dfe
Conrad <conrad.trohajr@gmail.com>
2019-03-16 16:15:57 -0400

    Created Race and Class Library Models

---
+++ ANightsTale/ANightsTale.Library/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANightsTale.Library
+{
+    public class Class
+    {
+        public int ClassID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ANightsTale/ANightsTale.Library/Race.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANightsTale.Library
+{
+    class Race
+    {
+        public int RaceID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
