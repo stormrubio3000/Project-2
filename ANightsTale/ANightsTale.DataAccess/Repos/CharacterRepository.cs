b669e5682647e98937baaa5c76885fad74a1cc99
Conrad <conrad.trohajr@gmail.com>
2019-03-16 16:33:37 -0400

    CharacterRepo and ICharacterRepo Created. No implementation

---
+++ ANightsTale/ANightsTale.DataAccess/Repos/CharacterRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ANightsTale.Library;
+
+namespace ANightsTale.DataAccess.Repos
+{
+    public class CharacterRepository : ICharacterRepository
+    {
+        public void CreateCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Character> GetAllCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character GetCharacterByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitialRolls(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RandomInitialRolls(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateModifiers(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateSavingThrows(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateSkills(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ANightsTale/ANightsTale.DataAccess/Repos/ICharacterRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANightsTale.DataAccess.Repos
+{
+    public interface ICharacterRepository
+    {
+        void CreateCharacter();
+        void DeleteCharacter();
+
+        IEnumerable<Library.Character> GetAllCharacters();
+        Library.Character GetCharacterById(int id);
+        Library.Character GetCharacterByName(string name);
+
+        void RandomInitialRolls(int id);
+        void InitialRolls(int id);
+        void CalculateModifiers(int id);
+        void CalculateSavingThrows(int id);
+        void CalculateSkills(int id);
+
+        void Save();
+    }
+}
