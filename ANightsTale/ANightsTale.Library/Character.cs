e60d095db62578cdf03dc34d128a3a976bdadb20
Conrad <conrad.trohajr@gmail.com>
2019-03-16 16:01:29 -0400

    Created Character and CharStats Library Models

---
+++ ANightsTale/ANightsTale.Library/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANightsTale.Library
+{
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public string Name { get; set; }
+        public int CampaignID { get; set; }
+        public int RaceID { get; set; }
+        public int ClassID { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public int Str { get; set; }
+        public int Dex { get; set; }
+        public int Con { get; set; }
+        public int Int { get; set; }
+        public int Wis { get; set; }
+        public int Cha { get; set; }
+        public int Speed { get; set; }
+        public int MaxHP { get; set; }
+    }
+}
