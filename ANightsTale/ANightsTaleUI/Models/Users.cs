d6f443cb5fa90824bff1a27420e4b444c48138f3
Daniel Rendon <danielrendon93@gmail.com>
2019-03-21 00:14:38 -0500

    adding UserController web API

---
+++ ANightsTale/ANightsTaleUI/Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANightsTaleAPI.Models
+{
+    public class Users
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Permission")]
+        public int Permission { get; set; }
+    }
+}
