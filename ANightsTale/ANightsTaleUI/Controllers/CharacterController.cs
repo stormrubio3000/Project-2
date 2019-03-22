94a407bdd115b179b73a66f8953dacc318e31df2
stormrubio3000 <storm-3000@hotmail.com>
2019-03-22 15:20:29 -0500

    Added Character API controller framewwork.

---
+++ ANightsTale/ANightsTaleUI/Controllers/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ANightsTale.DataAccess.Repos;
+using ANightsTale.Library;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANightsTaleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+
+		public CharacterRepository Repo { get; }
+
+		public CharacterController(CharacterRepository repo)
+		{
+			Repo = repo;
+		}
+
+
+		// GET: api/Character
+		[HttpGet]
+        public IEnumerable<Character> Get()
+        {
+			return Repo.GetAllCharacters();
+
+        }
+
+        // GET: api/Character/5
+        [HttpGet("{id}", Name = "GetCharacter")]
+        public Character Get(int id)
+        {
+			return Repo.GetCharacterById(id);
+		}
+
+        // POST: api/Character
+        [HttpPost]
+        public void Post([FromBody] Character value)
+        {
+			Repo.AddCharacter(value);
+			Repo.Save();
+        }
+    }
+}
