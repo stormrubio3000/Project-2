9681a0c73c918dbc1d9a16dc9b01b85f3c9c6672
stormrubio3000 <storm-3000@hotmail.com>
2019-03-26 13:55:31 -0500

    Added in the repo functions

---
+++ ANightsTale/ANightsTale.DataAccess/Repos/IAbilityRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ANightsTale.Library;
+
+namespace ANightsTale.DataAccess.Repos
+{
+	public interface IAbilityRepository
+	{
+		void CreateAbility(Library.Abilities ability);
+		void CreateCharAbilities(Library.CharAbilities ability);
+		void CreateCharFeat(Library.CharFeats feat);
+		void CreateFeat(Library.Feats feat);
+		void DeleteAbility(int id);
+		void DeleteFeat(int id);
+		Library.Abilities GetAbilityById(int id);
+		Library.Abilities GetAbilityByName(string name);
+		IEnumerable<Library.Abilities> GetAllAbilities();
+		IEnumerable<Library.CharAbilities> GetAllCharAbilities();
+		IEnumerable<Library.CharFeats> GetAllCharFeats();
+		IEnumerable<Library.Feats> GetAllFeats();
+		Library.CharAbilities GetCharAbilitiesById(int id);
+		Library.CharFeats GetCharFeatById(int id);
+		Library.Feats GetFeatById(int id);
+		Library.Feats GetFeatByName(string name);
+		void Save();
+		void UpdateAbility(Library.Abilities ability);
+		void UpdateFeat(Library.Feats feat);
+	}
+}
