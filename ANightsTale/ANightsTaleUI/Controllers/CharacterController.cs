666c44d624aae662dbf7b59bf586861a34899d28
Daniel Rendon <danielrendon93@gmail.com>
2019-03-26 13:45:27 -0500

    adding new methods to repository

--- ANightsTale/ANightsTaleUI/Controllers/CharacterController.cs
+++ ANightsTale/ANightsTaleUI/Controllers/CharacterController.cs
@@ -16,11 +16,13 @@ namespace ANightsTaleAPI.Controllers
 
 		public CharacterRepository Repo { get; }
         public UserRepository UserRepo { get; }
+        public CampaignRepository CampRepo { get; }
 
-        public CharacterController(CharacterRepository repo, UserRepository userRepo)
+        public CharacterController(CharacterRepository repo, UserRepository userRepo, CampaignRepository campRepo)
 		{
 			Repo = repo;
             UserRepo = userRepo;
+            CampRepo = campRepo;
 		}
 
 
@@ -41,17 +43,56 @@ namespace ANightsTaleAPI.Controllers
 
         // GET: api/Character/5
         [HttpGet("CharCampUsr/{id}", Name = "CharCampUsr")]
-        public IEnumerable<Character> Get(int id,[FromQuery] string username)
+        public IEnumerable<Models.Character> Get(int id,[FromQuery] string username)
         {
+            List<Models.Character> characters = new List<Models.Character>();
             int usrId = UserRepo.GetUserByUsername(username).UserID;
-            return Repo.GetCharacterByCampUsr(id, usrId);
+            foreach (var item in Repo.GetCharacterByCampUsr(id, usrId))
+            {
+                Models.Character character = new Models.Character();
+                character.Name = item.Name;
+                character.Bio = item.Bio;
+                character.Race = Repo.GetRaceById(item.RaceID).Name;
+                character.Class = Repo.GetClassById(item.ClassID).Name;
+                character.CampaignName = CampRepo.GetCampaignById(item.CampaignID).Name;
+                character.Experience = item.Experience;
+                character.Level = item.Level;
+                character.Str = item.Str;
+                character.Dex = item.Dex;
+                character.Con = item.Con;
+                character.Int = item.Int;
+                character.Wis = item.Cha;
+                character.Speed = item.Speed;
+                character.MaxHP = item.MaxHP;
+                characters.Add(character);
+            }
+            return characters;
         }
 
         [HttpGet("{id}", Name = "CharCamp")]
-        public IEnumerable<Character> GetCharCamp(int id)
+        public IEnumerable<Models.Character> GetCharCamp(int id)
         {
-
-            return Repo.GetCharacterByCamp(id);
+            List<Models.Character> characters = new List<Models.Character>();
+            foreach (var item in Repo.GetCharacterByCamp(id))
+            {
+                Models.Character character = new Models.Character();
+                character.Name = item.Name;
+                character.Bio = item.Bio;
+                character.Race = Repo.GetRaceById(item.RaceID).Name;
+                character.Class = Repo.GetClassById(item.ClassID).Name;
+                character.CampaignName = CampRepo.GetCampaignById(item.CampaignID).Name;
+                character.Experience = item.Experience;
+                character.Level = item.Level;
+                character.Str = item.Str;
+                character.Dex = item.Dex;
+                character.Con = item.Con;
+                character.Int = item.Int;
+                character.Wis = item.Cha;
+                character.Speed = item.Speed;
+                character.MaxHP = item.MaxHP;
+                characters.Add(character);
+            }
+            return characters;
         }
 
         // POST: api/Character
