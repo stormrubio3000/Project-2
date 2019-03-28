549993f0856098f1168a902b898f8103d30b80c8
Conrad <conrad.trohajr@gmail.com>
2019-03-28 14:14:07 -0400

    Created Skill class to help pass data, and GetSkillsByClass method.

---
+++ ANightsTale/ANightsTale.Library/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ANightsTale.Library
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Skill(int id, string name)
+        {
+            Id = id;
+            Name  =name;
+        }
+    }
+}
